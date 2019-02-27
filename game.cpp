03a74db0fabe8524e1270a67976f133423409e16
Sergey Kasmy <SleeplessSloth79@gmail.com>
2019-02-27 20:13:45 +0200

    Add a dynamic game field to Game class

--- game.cpp
+++ game.cpp
@@ -3,5 +3,15 @@
 
 Game::Game()
 {
-    printf("Hello World");
+    field = new int*[field_size];
+    for(int row = 0; row < field_size; row++)
+    {
+        field[row] = new int[field_size];
+    }
+}
+
+Game::~Game()
+{
+    for(int row = 0; row < field_size; row++) delete [] field[row];
+    delete [] field;
 }
--- game.hpp
+++ game.hpp
@@ -2,6 +2,10 @@
 
 class Game
 {
+    private:
+        int field_size = 15;
+        int** field;
     public:
         Game();
+        ~Game();
 };
