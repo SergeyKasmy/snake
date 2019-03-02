e1f31c1a3a03cdb82d9f6a87c2eef0d125fc9e9b
Sergey Kasmy <SleeplessSloth79@gmail.com>
2019-03-02 22:27:03 +0200

    Comment out "food" gen

--- game.cpp
+++ game.cpp
@@ -9,6 +9,7 @@ void Game::tick()
 
     while(true)
     {
+        // true if the time of the next tick(last tick + tick duration) is in the past
         if((last_tick + TICK_DURATION) < std::chrono::high_resolution_clock::now())
         {
             update();
@@ -19,6 +20,7 @@ void Game::tick()
 
 void Game::update()
 {
+    /*
     // pseudo-random number generator
     std::mt19937 rng;
     // generate a seed from a random device in the system
@@ -27,6 +29,8 @@ void Game::update()
     std::uniform_int_distribution<std::mt19937::result_type> dist(0, field->field_size - 1);
 
     field->set({(int) dist(rng), (int) dist(rng)}, 1);
+    */
+   
     ui.display_field(field);
 }
 
